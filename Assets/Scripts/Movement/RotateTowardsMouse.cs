d6ce123064f77d56a40d21416c39d669b275c784
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2025-12-14 16:31:40 +0200

    angle calc works

--- Assets/Scripts/Movement/RotateTowardsMouse.cs
+++ Assets/Scripts/Movement/RotateTowardsMouse.cs
@@ -1,18 +1,28 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotateTowardsMouse : MonoBehaviour
 {
     private Vector2 mousePos;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera mainCam;
+
+    private void OnValidate()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Awake()
     {
-        InputManager.OnLook += pos => { mousePos = pos; print(mousePos); };
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         
+        float angleSelfToMouse = Vector2.SignedAngle(transform.up, mousePos - (Vector2)transform.position);
+        print($"transform.up: {(Vector2)transform.up}");
+        print(angleSelfToMouse);
     }
 }
