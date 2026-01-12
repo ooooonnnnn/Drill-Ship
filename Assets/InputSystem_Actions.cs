c59d726dcd7b16b3a3665423d374313dbdcdb01c
CaptainSamulon <163521592+CaptainSamulon@users.noreply.github.com>
2026-01-12 13:18:46 +0200

    Tutorial text
    1. Changed the UI to scale with screen size
    2. Remapped reset from "E" to "R"
    3. Added controls explanation text:
    LMB: Drill
    RMB: Grappling hook:
      RMB(click) on an object to attach
      RMB(click) while the hook is out: Detach
      RMB(hold) while the hook is out: Reel-in(shorten)
    Long-press R to reset

--- Assets/InputSystem_Actions.cs
+++ Assets/InputSystem_Actions.cs
@@ -528,7 +528,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""1c04ea5f-b012-41d1-a6f7-02e963b52893"",
-                    ""path"": ""<Keyboard>/e"",
+                    ""path"": ""<Keyboard>/r"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
